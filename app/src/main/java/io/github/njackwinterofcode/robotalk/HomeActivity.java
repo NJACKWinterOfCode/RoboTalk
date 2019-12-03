151d4127d21a16561763d8f1d86c5db15c05434c
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2019-12-03 16:39:07 +0530

    Login and SignUp Activity added.

--- app/src/main/java/io/github/njackwinterofcode/robotalk/HomeActivity.java
+++ app/src/main/java/io/github/njackwinterofcode/robotalk/HomeActivity.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button tts;
     Button stt;
+    Button logOut;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,7 @@ public class HomeActivity extends AppCompatActivity {
 
         tts = findViewById(R.id.button1);
         stt = findViewById(R.id.button2);
+        logOut = findViewById(R.id.logOut);
 
         tts.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +37,15 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(HomeActivity.this,speechToText.class));
+            }
+        });
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+                Toast.makeText(HomeActivity.this, "Logged Out.", Toast.LENGTH_SHORT).show();
             }
         });
     }
