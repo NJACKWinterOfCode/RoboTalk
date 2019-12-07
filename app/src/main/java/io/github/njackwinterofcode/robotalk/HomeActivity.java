df9b7e8b98955796e360b1a3eed0bc3f88f40217
Sarthak2601 <53938155+Sarthak2601@users.noreply.github.com>
2019-12-07 23:23:21 +0530

    Login and SignUp Activity added. (#17)
    * Login and SignUp Activity added.
    
    * Added 1 file and Updated 1 file (#16)
    
    * Added 1 file and Updated 1 file
    
    All the components of all screen were not having a @string resource.Added a @string resource to every component.
    In the landscape orientation the button 2 was getting shifted a little left.Corrected this also.
    
    * Changed color.xml file
    
    Done as guided by you
    
    * Fully correct and updated Color.xml file
    
    The previous file is not correct in syntax this file is correct.
    
    * Login and SignUp Activity added.
    
    * Removed Spaces + String values through xml

--- app/src/main/java/io/github/njackwinterofcode/robotalk/HomeActivity.java
+++ app/src/main/java/io/github/njackwinterofcode/robotalk/HomeActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button tts;
     Button stt;
-
+    Button logOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,18 +22,25 @@ public class HomeActivity extends AppCompatActivity {
 
         tts = findViewById(R.id.button1);
         stt = findViewById(R.id.button2);
-
+        logOut = findViewById(R.id.logOut);
         tts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(HomeActivity.this, textToSpeech.class));
             }
         });
-
         stt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(HomeActivity.this,speechToText.class));
+            }
+        });
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+                Toast.makeText(HomeActivity.this, "Logged Out.", Toast.LENGTH_SHORT).show();
             }
         });
     }
